e0d36d702f110b04329749379b14c6d80af3da5d
BugHunter-X <askzenai@gmail.com>
2024-12-06 06:24:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {return <div>Home</div>}
+function About() {return <div>About</div>}
+function Contact() { return <div>Contact</div>}
+function NoMatch() { return <div>404</div>}
